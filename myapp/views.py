00b307a570ccffbdadd9a3b7e785ba7976113d3f
Moringa School <calebmutuku4@gmail.com>
2022-05-28 23:02:45 +0300

    ft:added the function to search image

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,4 @@
+from email import message
 from django.shortcuts import render
 from .models import Image,Category,Location
 
@@ -10,4 +11,20 @@ def display(requests):
 
 
     return render(requests,'index.html',{'all_images':all_images,'locations':locations,'categories':categories,'title':title})
+
+def search_results(request):
+    locations = Location.objects.all()
+    categories = Category.objects.all()
+    title = 'Search'
+    if 'searchby' in request.GET and request.GET["searchby"]:
+        search_term = request.GET.get("searchby")
+        searched_images=Image.search_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'index.html',{"message":message,"all_images": searched_images,'locations':locations,'categories':categories, 'title':title})
+
+    else:
+        message = "You haven't searched for any image"
+        return render(request, 'index.html',{"message":message, 'locations':locations,'categories':categories, 'title':title})
+
 
