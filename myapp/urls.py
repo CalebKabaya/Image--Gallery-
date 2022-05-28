8eb18c8885a711e002764eee40c344cd7aa132cc
Moringa School <calebmutuku4@gmail.com>
2022-05-29 01:08:33 +0300

    ft:added the dropdown menu ad their functions

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,7 +8,11 @@ from . import views
 
 urlpatterns=[
     re_path('^$',views.display,name = 'display'),
-    re_path(r'^search/', views.search_results, name='search_results')
+    re_path(r'^search/', views.search_results, name='search_results'),
+    re_path(r'^category/(\w+)', views.categories_display,name='categories_display'),
+    re_path(r'^location/(\w+)', views.location_display,name='location_display'),
+
+
 
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -21,10 +21,24 @@ def search_results(request):
         searched_images=Image.search_category(search_term)
         message = f"{search_term}"
 
-        return render(request, 'index.html',{"message":message,"all_images": searched_images,'locations':locations,'categories':categories, 'title':title})
+        return render(request, 'single.html',{"message":message,"all_images": searched_images,'locations':locations,'categories':categories, 'title':title})
 
     else:
         message = "You haven't searched for any image"
         return render(request, 'index.html',{"message":message, 'locations':locations,'categories':categories, 'title':title})
+
+
+def categories_display(request,category):
+    categories=Category.objects.all()
+    title = f"(category)"
+    category_results= Image.search_category(category)
+    return render(request,'index.html',{"title":title,"all_images":category_results,"categories":categories})
+
+def location_display(request,location):
+    locations=Location.objects.all()
+    title = f"(location)"
+    location_results= Image.search_location(location)
+    return render(request,'index.html',{"title":title,"all_images":location_results,"locations":locations})
+
 
 
