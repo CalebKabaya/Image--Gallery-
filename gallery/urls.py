8aab524155ac41362d3787f3d8b3911d661e6296
Moringa School <calebmutuku4@gmail.com>
2022-05-28 09:10:06 +0300

    added the configurations

--- gallery/urls.py
+++ gallery/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+# from django.urls import re_path
+from django.conf.urls import include
 from django.conf.urls import url
+
+
+
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'',include('myapp.urls'))
+
+
 ]
---
+++ myapp/urls.py
@@ -0,0 +1,10 @@
+# from django.urls import re_path
+from django.conf.urls import url
+
+
+from . import views
+
+urlpatterns=[
+    url('^$',views.welcome,name = 'welcome'),
+
+]
