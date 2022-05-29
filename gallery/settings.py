115847efef53b298bd92aac0d3b68cf07785550d
Moringa School <calebmutuku4@gmail.com>
2022-05-29 16:44:39 +0300

    bug fix on .env file

--- gallery/settings.py
+++ gallery/settings.py
@@ -10,8 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
+
 import os
 import django_heroku
+import dj_database_url
+from decouple import config,Csv
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -19,11 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 
-import os
-import django_heroku
-import dj_database_url
-from decouple import Csv
-from decouple import config
+
 
 
 MODE=config("MODE", default="dev")
