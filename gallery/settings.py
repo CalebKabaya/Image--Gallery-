7a01a95191fa8c0d1b1fdd88c73c99abf04cab8f
Moringa School <calebmutuku4@gmail.com>
2022-05-29 16:55:39 +0300

    bug fix on screte key

--- gallery/settings.py
+++ gallery/settings.py
@@ -57,7 +57,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'bck678g9b330k-%sbe)kaz36v&gudp)dw9d-qxmpp7+akf6d-h'
+SECRET_KEY = 'bck678g9b330k-%sbe)kaz36v&gudp)dw9d-qxmpp7+akf6d-h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
