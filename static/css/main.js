3ccf6232e920bb15d77c341717e49e4d3f9231b0
Moringa School <calebmutuku4@gmail.com>
2022-05-30 08:11:18 +0300

    st:styled the base .html

---
+++ static/css/main.js
@@ -0,0 +1,11 @@
+// let copyUrls = document.querySelectorAll(".copy-url");
+
+// for (shareLink of copyUrls) {
+//     shareLink.addEventListener("click", (e) => {
+//       toCopy =
+//         e.target.parentElement.previousElementSibling.firstElementChild
+//           .currentSrc;
+//       navigator.clipboard.writeText(toCopy);
+//       e.target.innerText = "Link Copied!";
+//     });
+//   }
